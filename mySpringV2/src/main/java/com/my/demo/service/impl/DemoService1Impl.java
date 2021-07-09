5093203a403c14470d91f9ae6253a77259ef5982
yangkaiqi <yangkaiqi191218@credithc.com>
2021-07-09 15:44:28 +0800

    整理

---
+++ mySpringV2/src/main/java/com/my/demo/service/impl/DemoService1Impl.java
@@ -0,0 +1,24 @@
+package com.my.demo.service.impl;
+
+import com.my.demo.action.DemoAction;
+import com.my.demo.service.DemoService;
+import com.my.spring.framework.annotation.MyAutowired;
+import com.my.spring.framework.annotation.MyService;
+
+/**
+ * @ClassName DemoServiceImpl
+ * @Description TODO
+ * @Author ykq
+ * @Date 2020/4/29
+ * @Version v1.0.0
+ */
+@MyService
+public class DemoService1Impl implements DemoService {
+    @MyAutowired
+    private DemoAction demoAction;
+
+    @Override
+    public String get(String name) {
+        return "My name is " + name;
+    }
+}
